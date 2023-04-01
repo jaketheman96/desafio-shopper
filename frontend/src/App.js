ff8a2fb16945a09c36051243214be55980ac0bd7
Jake Chien <jake_chien@outlook.com>
2023-04-01 19:06:13 -0300

    feat: frontend - add's login and products page as path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './pages/Login';
+import Products from './pages/Products';
 
 function App() {
   return (
     <div className="App">
-      Shopper frontend App
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/products' element={<Products />} />
+      </Routes>
     </div>
   );
 }
