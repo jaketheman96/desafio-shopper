54bba88dd72d64761e439f4011b00298b35c8ba5
Jake Chien <jake_chien@outlook.com>
2023-04-02 01:27:35 -0300

    feat: frontend - implements login page

--- frontend/src/context/ShopperProvider.js
+++ frontend/src/context/ShopperProvider.js
@@ -1,4 +1,5 @@
 import { useState, useMemo } from 'react';
+import PropTypes from 'prop-types';
 import ShopperContext from './ShopperContext';
 
 function ShopperProvider({ children }) {
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 function Login() {
   return (
-    <div>Login page</div>
+    <section>
+      <form>
+        <label htmlFor="email">
+          Email:
+          <input type="email" name="email" />
+        </label>
+        <label htmlFor="password">
+          Password:
+          <input type="password" name="password" />
+        </label>
+        <button type="button">Ainda não tenho conta</button>
+        <button type="submit">Entrar</button>
+      </form>
+    </section>
   );
 }
 
