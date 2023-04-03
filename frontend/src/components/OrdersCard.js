23743a1fba157bebcc9734660d442cb5ad938e2c
Jake Chien <jake_chien@outlook.com>
2023-04-03 14:50:03 -0300

    feat: frontend - implements orders page

--- frontend/src/components/OrdersCard.js
+++ frontend/src/components/OrdersCard.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-function OrdersCard() {
+function OrdersCard({ index, id, status, totalPrice, saleDate }) {
+  const date = new Date(saleDate);
+  const fullDate = date.toLocaleDateString('pt-br', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+  const TWO_ZEROS = 2;
+
   return (
-    <section>OrdersCard</section>
+    <article>
+      <Link to={ `/order/details/${id}` }>
+        <p>{`Pedido: ${String(index + 1).padStart(TWO_ZEROS + 1, '0')}`}</p>
+        <p>{`Status: ${status}`}</p>
+        <p>{`R$${totalPrice.replace('.', ',')}`}</p>
+        <p>{fullDate}</p>
+      </Link>
+    </article>
   );
 }
+
+OrdersCard.propTypes = {
+  index: PropTypes.number.isRequired,
+  id: PropTypes.number.isRequired,
+  status: PropTypes.string.isRequired,
+  totalPrice: PropTypes.string.isRequired,
+  saleDate: PropTypes.string.isRequired,
+};
 
 export default OrdersCard;
--- frontend/src/pages/Orders.js
+++ frontend/src/pages/Orders.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Navbar from '../components/Navbar';
 import OrdersCard from '../components/OrdersCard';
+import ShopperContext from '../context/ShopperContext';
+import { handleAllFetchMethods } from '../utils/handleAllFetchMethods';
 
 function Orders() {
+  const { userInfos } = useContext(ShopperContext);
+  const [allUserOrders, setAllUserOrders] = useState([]);
+  const [showError, setShowError] = useState('');
+
+  useEffect(() => {
+    const getUserOrders = async () => {
+      if (userInfos) {
+        const orders = await handleAllFetchMethods(
+          `/sales/user/${userInfos.id}`,
+          'GET',
+          null,
+          userInfos.token,
+        );
+        if (orders.message) return setShowError(orders.message);
+        return setAllUserOrders(orders);
+      }
+    };
+    getUserOrders();
+  }, [userInfos]);
+
   return (
     <>
       <Navbar />
-      <OrdersCard />
+      <section>
+        {allUserOrders.length !== 0 && allUserOrders.map((order, index) => (
+          <OrdersCard
+            key={ order.id }
+            index={ index }
+            id={ order.id }
+            saleDate={ order.saleDate }
+            status={ order.status }
+            totalPrice={ order.totalPrice }
+          />
+        ))}
+      </section>
+      <p>{showError}</p>
     </>
   );
 }
