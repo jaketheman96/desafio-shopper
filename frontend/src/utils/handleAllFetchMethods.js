de4491092db929fa9b56dc380f8d4c9020355d70
Jake Chien <jake_chien@outlook.com>
2023-04-10 01:56:21 -0300

    deploy: frontend - changing endpoint on fetch

--- frontend/src/utils/handleAllFetchMethods.js
+++ frontend/src/utils/handleAllFetchMethods.js
@@ -1,3 +1,6 @@
+const HOST = process.env.REACT_APP_API_HOST || "localhost:3001";
+const PROTOCOL = process.env.REACT_APP_API_PROTOCOL || "http";
+
 const handleAllFetchMethods = async (route, fetchMethod, body, token) => {
   const requestOptions = {
     method: fetchMethod,
@@ -13,7 +16,7 @@ const handleAllFetchMethods = async (route, fetchMethod, body, token) => {
     delete requestOptions.body;
   }
   try {
-    const response = await fetch(`http://localhost:3001${route}`, requestOptions);
+    const response = await fetch(`${PROTOCOL}://${HOST}${route}`, requestOptions);
     const fetchData = await response.json();
     return fetchData;
   } catch (err) {
