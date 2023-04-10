47d8dfe510d416ae644dce2cb25fca55c03f8dfa
Jake Chien <jake_chien@outlook.com>
2023-04-10 02:08:44 -0300

    fix: frontend - fix backend endpoint to fetch in deploy

--- frontend/src/utils/handleAllFetchMethods.js
+++ frontend/src/utils/handleAllFetchMethods.js
@@ -1,6 +1,3 @@
-const HOST = process.env.REACT_APP_API_HOST || "localhost:3001";
-const PROTOCOL = process.env.REACT_APP_API_PROTOCOL || "http";
-
 const handleAllFetchMethods = async (route, fetchMethod, body, token) => {
   const requestOptions = {
     method: fetchMethod,
@@ -16,7 +13,7 @@ const handleAllFetchMethods = async (route, fetchMethod, body, token) => {
     delete requestOptions.body;
   }
   try {
-    const response = await fetch(`${PROTOCOL}://${HOST}${route}`, requestOptions);
+    const response = await fetch(`https://shopper-backend.up.railway.app${route}`, requestOptions);
     const fetchData = await response.json();
     return fetchData;
   } catch (err) {
